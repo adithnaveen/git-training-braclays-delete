57dc8df5cdd20579d706156dd3d0eeab146ab8c7
Naveen <adith.naveen@gmail.com>
2021-09-16 11:16:14 +0530

    updated login method for connection

--- Hello.java
+++ Hello.java
@@ -2,14 +2,16 @@ package com.naveen;
 
 class Hello  {
 	public void hi() {
-	
+		// i'm editing in main 
 	}
 
 
 	/** Author : Naveen */
-	public void login() {
+	public void login(Login login) {
 		// your login logic goes here 
+		Connection con = new Connection();
 	}
+	
 
 }
 
